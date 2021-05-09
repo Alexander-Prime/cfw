b4cce3f49f780cd0a9cce4d3ded0a73b6ece6d2d
Alex Peters <alexander.n.peters@gmail.com>
2021-05-08 19:45:56 -0700

    Add stub Controller struct

---
+++ src/controller.rs
@@ -0,0 +1,147 @@
+pub struct Frame {
+    pub left_quad_n: bool,
+    pub left_quad_e: bool,
+    pub left_quad_s: bool,
+    pub left_quad_w: bool,
+
+    pub right_quad_n: bool,
+    pub right_quad_e: bool,
+    pub right_quad_s: bool,
+    pub right_quad_w: bool,
+
+    pub left_pad_x: f32,
+    pub left_pad_y: f32,
+    pub left_pad_click: bool,
+
+    pub right_pad_x: f32,
+    pub right_pad_y: f32,
+    pub right_pad_click: bool,
+
+    pub home: bool,
+
+    pub select: bool,
+    pub start: bool,
+
+    pub left_bumper: bool,
+    pub right_bumper: bool,
+
+    pub left_trigger: f32,
+    pub right_trigger: f32,
+
+    pub left_grip: bool,
+    pub right_grip: bool,
+
+    pub accel_x: f32,
+    pub accel_y: f32,
+    pub accel_z: f32,
+
+    pub gyro_x: f32,
+    pub gyro_y: f32,
+    pub gyro_z: f32,
+
+    pub mag_x: f32,
+    pub mag_y: f32,
+    pub mag_z: f32,
+}
+
+impl core::fmt::Display for Frame {
+    fn fmt(&self, formatter: &mut core::fmt::Formatter) -> core::fmt::Result {
+        formatter.write_fmt(format_args!(
+            "\nQuads: left(N {}, E {}, S {}, W {}) right(N {}, E {}, S {} W {})",
+            self.left_quad_n,
+            self.left_quad_e,
+            self.left_quad_s,
+            self.left_quad_w,
+            self.right_quad_n,
+            self.right_quad_e,
+            self.right_quad_s,
+            self.right_quad_w,
+        ))?;
+        formatter.write_fmt(format_args!(
+            "\nPads: left(X {:.02}, Y {:.02}, click {}) right(X {:.02}, Y {:.02}, click {})",
+            self.left_pad_x,
+            self.left_pad_y,
+            self.left_pad_click,
+            self.right_pad_x,
+            self.right_pad_y,
+            self.right_pad_click,
+        ))?;
+        formatter.write_fmt(format_args!(
+            "\nselect {}, home {}, start {}",
+            self.select, self.home, self.start,
+        ))?;
+        formatter.write_fmt(format_args!(
+            "\nBumpers: left {}, right {}",
+            self.left_bumper, self.right_bumper,
+        ))?;
+        formatter.write_fmt(format_args!(
+            "\nTriggers: left {}, right {}",
+            self.left_trigger, self.right_trigger,
+        ))?;
+        formatter.write_fmt(format_args!(
+            "\nGrips: left {}, right {}",
+            self.left_grip, self.right_grip,
+        ))?;
+        Ok(())
+    }
+}
+
+pub struct Controller;
+
+impl Controller {
+    pub fn new() -> Self {
+        Controller
+    }
+}
+
+impl Iterator for Controller {
+    type Item = Frame;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        Some(Frame {
+            left_quad_n: false,
+            left_quad_e: false,
+            left_quad_s: false,
+            left_quad_w: false,
+
+            right_quad_n: false,
+            right_quad_e: false,
+            right_quad_s: false,
+            right_quad_w: false,
+
+            left_pad_x: 0.0,
+            left_pad_y: 0.0,
+            left_pad_click: false,
+
+            right_pad_x: 0.0,
+            right_pad_y: 0.0,
+            right_pad_click: false,
+
+            home: false,
+
+            select: false,
+            start: false,
+
+            left_bumper: false,
+            right_bumper: false,
+
+            left_trigger: 0.0,
+            right_trigger: 0.0,
+
+            left_grip: false,
+            right_grip: false,
+
+            accel_x: 0.0,
+            accel_y: 0.0,
+            accel_z: 0.0,
+
+            gyro_x: 0.0,
+            gyro_y: 0.0,
+            gyro_z: 0.0,
+
+            mag_x: 0.0,
+            mag_y: 0.0,
+            mag_z: 0.0,
+        })
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,11 @@
 #![no_main]
 #![feature(never_type)]
 
+mod controller;
 mod driver;
 mod logging;
 
-use bsp::hal::ccm::spi::{ClockSelect, PrescalarSelect};
-use driver::imu::Lsm6ds33;
-use firmware::input::{Acceleration, Rotation};
-use firmware::update::Update;
+use controller::Controller;
 use teensy4_bsp as bsp;
 use teensy4_panic as _;
 
@@ -19,48 +17,10 @@ fn main() -> ! {
     let mut systick = bsp::SysTick::new(c_peripherals.SYST);
     systick.delay(500);
 
-    let mut peripherals = bsp::Peripherals::take().unwrap();
-    let pins = bsp::t41::into_pins(peripherals.iomuxc);
+    let controller = Controller::new();
 
-    // Get 4 SPI module builders
-    // Not sure why we take the last one instead of, say, the first
-    // Examples do it this way, so we do too for now
-    let (_, _, _, spi4_builder) = peripherals.spi.clock(
-        &mut peripherals.ccm.handle,
-        ClockSelect::Pll2,
-        PrescalarSelect::LPSPI_PODF_7,
-    );
-
-    // Create the LSM6DS33 driver
-    let sixaxis = {
-        let mut cs = bsp::hal::gpio::GPIO::new(pins.p10).output();
-        cs.set();
-        let mut sixaxis_spi = spi4_builder.build(pins.p11, pins.p12, pins.p13);
-        sixaxis_spi.set_mode(embedded_hal::spi::MODE_3).unwrap();
-        Lsm6ds33::try_new(sixaxis_spi, cs)
-            .unwrap_or_else(|_| panic!("Failed to create LSM6DS33 driver"))
-    };
-
-    let (x, y, z, pitch, roll, yaw) = sixaxis.primitives();
-
-    let acceleration = Acceleration { x, y, z };
-    let rotation = Rotation { pitch, roll, yaw };
-
-    sixaxis.update();
-
-    for ((x, y, z), (pitch, roll, yaw)) in Iterator::zip(acceleration, rotation) {
-        if (x, y, z, pitch, roll, yaw) != (0.0, 0.0, 0.0, 0.0, 0.0, 0.0) {
-            log::info!(
-                "A({:.2}, {:.2}, {:.2}) R({:.2}, {:.2}, {:.2})",
-                x,
-                y,
-                z,
-                pitch,
-                roll,
-                yaw
-            );
-        }
-        sixaxis.update();
+    for frame in controller {
+        log::info!("{}", frame);
     }
 
     log::info!("Iterator ended, halting");
